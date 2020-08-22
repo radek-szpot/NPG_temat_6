cd7857e5ca20808e04aa01e0d711c9daaba118ec
Radoslaw Szpot <rszpot@student.agh.edu.pl>
2020-08-22 15:42:45 +0200

    quick fix

--- program.py
+++ program.py
@@ -5,7 +5,7 @@ from add_new_task import add_new_task
 #from print_tasks import print_tasks
 
 def start():
-    print("Napisz liczbe od 1 do 2 by wybrac dana akcje: \n 1.Dodanie nowego zadania \n 2.Wypisanie zadan")
+    print("Napisz liczbe od 1 do 3 by wybrac dana akcje: \n 1.Dodanie nowego zadania \n 2.Wypisanie zadan \n 3.Zamkniecie programu")
     action = int(input())
     next_action(action)
 
@@ -17,7 +17,10 @@ def next_action(action=0):
         print("Ile zadan ma zostac wypisane?")
         task_amount = int(input())
         # print_tasks(task_amount) TODO:BASIA
+    elif action == 3:
+        # zapisz akutalna liste zadan TODO:RADEK
+        exit()
     else:
-        print("Prosze podac liczbe od 1 do 2")
+        print("Prosze podac liczbe od 1 do 3")
         action = int(input())
         next_action(action)
