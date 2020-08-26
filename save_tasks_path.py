905e7f3b23145df372b636a9d510d5d51a977130
Michał Spinczyk <email@example.com>
2020-08-26 21:54:56 +0200

    Fixed some problems

--- save_tasks_path.py
+++ save_tasks_path.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 import json
 
 def save_to_path(path, fileName, json_data):
     filePathName = path + fileName + '.json'
     with open(filePathName, 'w') as f:
-        f.write(json.dumps(json_data))
+        f.write(json.dumps(json_data, indent=4))
 
 
 #tak to ma mniej więcej wygladac???
