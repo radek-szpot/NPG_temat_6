d4844dcb6f3382038a4b248bdf5296cbfd37ffd9
Anna Bernacka <annabernacka@student.agh.edu.pl>
2020-08-27 19:29:26 +0200

    Added another solution to the task

--- edit_Tasks.py
+++ edit_Tasks.py
@@ -1,5 +1,4 @@
 #funkcjonalnosc 5- edytowanie zadan
-#nie działa do końca tak jakbym chciała ale to jeszcze do tego wroce
 #zmienia sie tylko jedna cecha w jednym zadaniu narazie
 
 from Tasks import Tasks
@@ -25,3 +24,35 @@ def edit_tasks (ListOfTasks):
         d += 1
 
     print(ListOfTasks)
+
+
+#Podobnie jak w poprzednim pliku delete_Tasks.py tez znalazlam inne rozwiazanie i tez zostawie oba rozwiazania narazie
+#Działa na podobnej zasadzie co ten program z usuwaniem tylko zastepuje poprzednie informacje nowymi
+
+def edit_tasks ():
+    view_data ()
+    new_data = []
+    with open (filename, "r") as f:
+        tasks = json.load(f)
+        data_length = len(tasks) - 1
+    print ("Type id number in order to delete task")
+    edit_option = input(f"Select a number 0-(data_length)")
+    i = 0
+    for entry in tasks:
+        if i == int(edit_option):
+            name = entry["name"]
+            importance = entry["importance"]
+            description = entry["decsription"]
+            print("Current name : {name}")
+            name = input("Type in new name")
+            print("Current name : {importance}")
+            importance = input("Type in new importance")
+            print("Current name : {description}")
+            description = input("Type in new description")
+            new_data.append({"name" : name, "importance" : importance, "description" : description})
+            i = i+1
+        else:
+            new_data.append(entry)
+            i= i+1
+    with open (filename, "w") as f:
+        json.dump(new_data, f)
