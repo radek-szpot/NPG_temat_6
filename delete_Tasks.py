5cf699707fb56177d8772e34e33a03468168f505
Anna Bernacka <annabernacka@student.agh.edu.pl>
2020-09-03 19:39:49 +0200

    Added changes, changed statements to polish language

--- delete_Tasks.py
+++ delete_Tasks.py
@@ -8,7 +8,7 @@ from parse_input_for_int import parse_input_for_int
 
 def delete_tasks(list_of_tasks):
     print_tasks_not_sorted(list_of_tasks, len(list_of_tasks))
-    print("Type task id in order to delete it")
+    print("Podaj id zadania, które chcesz usunąć")
     n = parse_input_for_int(1, len(list_of_tasks))
     a = 0
     for i in list_of_tasks:
@@ -16,6 +16,6 @@ def delete_tasks(list_of_tasks):
             del (list_of_tasks[a])
         a += 1
 
-    print("Task has been deleted")
+    print("Zadanie o podanym id zostało usunięte")
     print_tasks_not_sorted(list_of_tasks, len(list_of_tasks))
     return list_of_tasks
--- edit_Tasks.py
+++ edit_Tasks.py
@@ -4,31 +4,20 @@ Module which allows user to edit tasks
 
 from print_tasks import print_tasks_not_sorted
 
+from parse_input_for_int import parse_input_for_int
+
 
 def edit_tasks(list_of_tasks):
     print_tasks_not_sorted(list_of_tasks, len(list_of_tasks))
-    print("Type task id in order to edit it")
-    n = int(input())
-    d = 0
-    for i in list_of_tasks:
-        if i["id"] == n:
-            # a - name
-            # b - importance
-            # c - descripton
-            a = list_of_tasks[d]["name"]
-            b = list_of_tasks[d]["importance"]
-            c = list_of_tasks[d]["description"]
-            print("Current data: \nname: %s, \nimportance: %s, \ndescription: %s",
-                  list_of_tasks[d]["name"], list_of_tasks[d]["importance"], list_of_tasks[d]["description"])
-            # Nie wiem czy to juz nie jest troche za duzo kombinowania ale tak chyba lepiej wygladac
-            a = input("Type in new name")
-            b = input("Type in new importance")
-            c = input("Type in new description")
-            list_of_tasks[d]["name"] = a
-            list_of_tasks[d]["importance"] = b
-            list_of_tasks[d]["description"] = c
-            print("Data has been changed for task id: %d", n)
-    d += 1
+    print("Podaj numer zadania, które chcesz edytować")
+    user_edit_choice = parse_input_for_int(1, len(list_of_tasks))
+
+    print("Podaj nową nazwę zadania:")
+    list_of_tasks[user_edit_choice - 1]["name"] = input()
+    print("Podaj nowy opis zadania:")
+    list_of_tasks[user_edit_choice - 1]["description"] = input()
+    print("Podaj nowy priorytet zadania (liczba 1, 2 lub 3):")
+    list_of_tasks[user_edit_choice - 1]["importance"] = parse_input_for_int(1, 3)
 
     print_tasks_not_sorted(list_of_tasks, len(list_of_tasks))
     return list_of_tasks
