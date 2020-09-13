8ab17add006b71d8e73f59a8d2380c192a0b75e0
b-pobiedzinska <64206685+b-pobiedzinska@users.noreply.github.com>
2020-09-13 17:05:12 +0200

    Unifying names
    json_data to list_of_tasks

--- print_tasks.py
+++ print_tasks.py
@@ -5,22 +5,22 @@ from parse_input_for_int import parse_input_for_int
 import colours
 
 
-def print_tasks(json_data):
+def print_tasks(list_of_tasks):
     print("Czy wyświetlić zadania posortowane?\n",
           "0. Nie\n",
           "1. Tak")
     whether_sort = parse_input_for_int(0, 1)
     if whether_sort == 0:
         print('Ile zadań chcesz wypisać? Jeżeli chcesz wypisać wszystkie napisz "0"')
-        task_amount = parse_input_for_int(0, len(json_data))
+        task_amount = parse_input_for_int(0, len(list_of_tasks))
         if task_amount == 0:
-            task_amount = len(json_data)
-        print_tasks_not_sorted(json_data, task_amount)
+            task_amount = len(list_of_tasks)
+        print_tasks_not_sorted(list_of_tasks, task_amount)
     elif whether_sort == 1:
-        print_tasks_sorted(json_data)
+        print_tasks_sorted(list_of_tasks)
 
 
-def print_tasks_sorted(json_data):
+def print_tasks_sorted(list_of_tasks):
     # Print sorted
     print("Jak chcesz posortować zadania?\n",
           "1. Według daty dodania\n",
@@ -30,7 +30,7 @@ def print_tasks_sorted(json_data):
     # Sorting chronologicaly
     if sorting_type == 1:
         ordered_data = {}
-        for i in json_data:
+        for i in list_of_tasks:
             ordered_data[i['id']] = i
         ordered_data_sorted = sorted(ordered_data)
         sorted_list = [ordered_data[i] for i in ordered_data_sorted]
@@ -42,19 +42,19 @@ def print_tasks_sorted(json_data):
 
     # Sorting by priority
     elif sorting_type == 2:
-        for i in json_data:
+        for i in list_of_tasks:
             if i['importance'] == 1:
                 colours.prGreen(i['name'])
                 print(' -', i['description'])
             else:
                 continue
-        for i in json_data:
+        for i in list_of_tasks:
             if i['importance'] == 2:
                 colours.prYellow(i['name'])
                 print(' -', i['description'])
             else:
                 continue
-        for i in json_data:
+        for i in list_of_tasks:
             if i['importance'] == 3:
                 colours.prRed(i['name'])
                 print(' -', i['description'])
@@ -63,9 +63,9 @@ def print_tasks_sorted(json_data):
         return
 
 
-def print_tasks_not_sorted(json_data, task_amount):
+def print_tasks_not_sorted(list_of_tasks, task_amount):
     break_when_n = 1
-    for i in json_data:
+    for i in list_of_tasks:
         if i['importance'] == 1:
             print(i['id'], ".", end='')
             colours.prGreen(i['name'])
