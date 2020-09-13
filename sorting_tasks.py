a159da58e027614e3370128f88cb5331cfebb6fb
b-pobiedzinska <64206685+b-pobiedzinska@users.noreply.github.com>
2020-09-13 17:10:34 +0200

    Unifying names
    json_data to list_of_tasks

--- sorting_tasks.py
+++ sorting_tasks.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #optional functionality
 import json
-def chronological_sort_oldest(json_data):
-    return json_data   #if id will always start from the biggest one +1 it will always be ordered chronologically 
+def chronological_sort_oldest(list_of_tasks):
+    return list_of_tasks   #if id will always start from the biggest one +1 it will always be ordered chronologically 
 
-def chronological_sort_latest(json_data): # here you only need to reverse the list of tasks
+def chronological_sort_latest(list_of_tasks): # here you only need to reverse the list of tasks
